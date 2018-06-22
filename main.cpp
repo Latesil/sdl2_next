5bc78eca4406628f64672f2171c74bd19789dd06
Latesil <skipastas@gmail.com>
2018-06-22 14:16:36 +0300

    simple window

--- main.cpp
+++ main.cpp
@@ -5,6 +5,46 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!2255" << endl;
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        cout << "SDL_Init_Error: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == nullptr) {
+        cout << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr) {
+        cout << "SDL_CreateRenderer Error: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    SDL_Surface* bmp = SDL_LoadBMP("/home/stas/qt-creator/projects/sdl2_next/hello.bmp");
+    if (bmp == nullptr) {
+        cout << "SDL_LoadBMP Error: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
+    SDL_FreeSurface(bmp);
+    if (tex == nullptr) {
+        cout << "SDL_CreateTextureFromSurface Error " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    SDL_RenderClear(ren);
+    SDL_RenderCopy(ren, tex, NULL, NULL);
+    SDL_RenderPresent(ren);
+
+    SDL_Delay(2000);
+
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
     return 0;
 }
